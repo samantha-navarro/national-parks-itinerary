716da3ddc579d0a8d5a80f3f68f974d05de63122
Samantha Navarro <samantha.m.navarro@gmail.com>
2022-08-18 15:20:23 -0700

    fixed phto scroll on background photo and changed styling for navbar

--- client/src/components/css/ItineraryNavBar.js
+++ client/src/components/css/ItineraryNavBar.js
@@ -1,45 +1,35 @@
+import React from 'react'
 import React, { useState } from "react";
 import { Link, Navigate } from 'react-router-dom';
 import { FaBars, FaTimes } from "react-icons/fa"; 
 import { blueGrey } from "@mui/material/colors";
 import './css/NavBar.css'
 
-export default function NavBar ({ setCurrentUser }) {
+
+export default function ItineraryNavBar() {
+
     const [click, setClick] = useState(false)
     const handleClick = () => setClick(!click)
 
-    //logout button
-    const handleLogOut = () => {
-        //DELETE '/logout'
-        fetch('/logout', {
-            method: "DELETE"
-        })
-        setCurrentUser(false)
-        localStorage.clear();
-        Navigate('/')
-    };
-
-    return (
-        <div className="header">
+  return (
+    <div>
+             <div className="header">
             {/* <Link to="/main"><h1>National Parks Itinerary</h1></Link> */}
             <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                 <li>
                     <Link to="/main">HOME</Link>
                 </li>
                 <li>
-                    <Link to="/parks">PARKS</Link>
-                </li>
-                <li>
-                    <Link to="/itinerary">ITINERARY</Link>
+                    <Link to="/">PARKS</Link>
                 </li>
                 <li>
                     <Link onCLick={handleLogOut} to="/">LOGOUT</Link>
                 </li>
             </ul>
             <div className="icons" onClick={handleClick}>
-            {click ? (<FaTimes size={20} style={{color: blueGrey}} />) : 
-            (<FaBars size={20} style={{color: blueGrey}} />)}
+            {click ? (<FaTimes size={20} style={{color: "blueGrey"}} />) : (<FaBars size={20} style={{color: "blueGrey"}} />)}
             </div>
         </div>
-    )
+    </div>
+  )
 }
